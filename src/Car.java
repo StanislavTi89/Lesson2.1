d9a41991675878c0c011371d98cd5d5336fe0662
StanislavTi89 <Timoshenko311989@gmail.com>
2023-01-15 11:14:44 +0300

    ООП_Введение

---
+++ src/Car.java
@@ -0,0 +1,18 @@
+public class Car {
+    private String brand;
+    private String model;
+    private String engineVolume;
+    private String color;
+    private String year;
+    private String country;
+
+    public Car(String brand, String model, String engineVolume, String year, String country, String color) {
+        this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.color = color;
+        this.year = year;
+        this.country = country;
+
+    }
+}
